ec8a43d94c7506404c446d5ce4275e9997f934be
robshock <robshock16@gmail.com>
2026-05-17 23:35:50 -0600

    Add files via upload

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 const planeGreen1 = document.getElementById("planeGreen1");
 const planeYellow1 = document.getElementById("planeYellow1");
 const planeBlue1 = document.getElementById("planeBlue1");
-const planeX = window.innerWidth * 0.08;
-//const planeY = window.innerWidth * 0.08;
+const planeX1 = window.innerWidth * 0.455;
+const planeY1 = window.innerWidth * 0.001
+const planeX2 = window.innerWidth * 0.43;
+const planeY2 = window.innerWidth * 0.001
 const speed = 2;
 
 const button1 = document.getElementById("arrowBtn");
@@ -11,46 +13,108 @@ const img1 = document.getElementById("arrowImg");
 const button2 = document.getElementById("arrowBtn2");
 const img2 = document.getElementById("arrowImg2");
 
+const button3 = document.getElementById("arrowBtn3");
+const img3 = document.getElementById("arrowImg3");
+
+const button4 = document.getElementById("arrowBtn4");
+const img4 = document.getElementById("arrowImg4");
+
+const button5 = document.getElementById("arrowBtn5");
+const img5 = document.getElementById("arrowImg5");
+
+const button6 = document.getElementById("arrowBtn6");
+const img6 = document.getElementById("arrowImg6");
+
+const button7 = document.getElementById("arrowBtn7");
+const img7 = document.getElementById("arrowImg7");
+
+const button8 = document.getElementById("arrowBtn8");
+const img8 = document.getElementById("arrowImg8");
+
+const button9 = document.getElementById("arrowBtn9");
+const img9 = document.getElementById("arrowImg9");
+
+
 // GREEN PLANE
-let x = planeX, y = 0; 
-let verticalLock = false;
-let firstDecisionMade = false;
-let secondDecisionMade = false;
+let greenX = planeX1, greenY = planeY1; 
+let verticalLockGreen = false;
+let horizontalLockGreen = false;
+let firstDecisionMadeGreen = false;
+let secondDecisionMadeGreen = false;
+let thirdDecisionMadeGreen = false;
+let fourthDecisionMadeGreen = false;
+let fifthDecisionMadeGreen = false;
+let sixDecisionMadeGreen = false;
+let seventhDecisionMadeGreen = false;
+let eighthDecisionMadeGreen = false;
+let ninthDecisionMadeGreen = false;
 
 // YELLOW PLANE
-let x2 = -250, y2 = 0; //!
-let verticalLock2 = false;
-let firstDecisionMade2 = false;
-let secondDecisionMade2 = false;
+let yellowX = planeX2, yellowY = planeY2; 
+let verticalLockYellow = false;
+let horizontalLockYellow = false;
+let firstDecisionMadeYellow = false;
+let secondDecisionMadeYellow = false;
+let thirdDecisionMadeYellow = false;
+let fourthDecisionMadeYellow = false;
+let fifthDecisionMadeYellow = false;
+let sixDecisionMadeYellow = false;
+let seventhDecisionMadeYellow = false;
+let eighthDecisionMadeYellow = false;
+let ninthDecisionMadeYellow = false;
 
-// BLUE PLANE
-let x3 = -250, y3 = 0; //!
-let verticalLock3 = false;
-let firstDecisionMade3 = false;
-let secondDecisionMade3 = false;
 
 // SETTINGS
 let decisionPoint1;
 let decisionPoint2;
+let decisionPoint3;
+let decisionPoint4;
+let decisionPoint5;
+let decisionPoint6;
+let decisionPoint7;
+let decisionPoint8;
+let decisionPoint9;
+
 
 function updateDecisionPoints() {
-  decisionPoint1 = window.innerHeight * 0.3; //!
-  decisionPoint2 = window.innerHeight * 0.7; //!
+  decisionPoint1 = window.innerHeight * 0.4;
+  decisionPoint2 = window.innerHeight * 0.5;
+  decisionPoint3 = window.innerHeight * 0.6;
+  decisionPoint4 = window.innerHeight * 0.8;
+  decisionPoint5 = window.innerHeight * 0.7;
+  decisionPoint6 = window.innerHeight * 0.7;
+  decisionPoint7 = window.innerHeight * 0.7;
+  decisionPoint8 = window.innerHeight * 0.7;
+  decisionPoint9 = window.innerHeight * 0.7;
 }
 
 
 // BUTTONS
 let mode1Right = false;
-let mode2Right = false;
 let mode1Right_2 = false;
+let mode2Right = false;
 let mode2Right_2 = false;
+let mode3Right = false;
+let mode3Right_2 = false;
+let mode4Right = false;
+let mode4Right_2 = false;
+let mode5Right = false;
+let mode5Right_2 = false;
+let mode6Right = false;
+let mode6Right_2 = false;
+let mode7Right = false;
+let mode7Right_2 = false;
+let mode8Right = false;
+let mode8Right_2 = false;
+let mode9Right = false;
+let mode9Right_2 = false;
 
 button1.addEventListener("click", () => {
   mode1Right = !mode1Right;
 
   img1.src = mode1Right
     ? "pngs/arrowUp.png"
-    : "pngs/arrowRight.png";
+    : "pngs/arrowLeft.png";
 
   mode1Right_2 = mode1Right;
 });
@@ -60,27 +124,97 @@ button2.addEventListener("click", () => {
 
   img2.src = mode2Right
     ? "pngs/arrowUp.png"
-    : "pngs/arrowRight.png";
+    : "pngs/arrowLeft.png";
 
   mode2Right_2 = mode2Right;
+});
+
+button3.addEventListener("click", () => {
+  mode3Right = !mode3Right;
+
+  img3.src = mode3Right
+    ? "pngs/arrowUp.png"
+    : "pngs/arrowLeft.png";
+
+  mode3Right_2 = mode3Right;
+});
+
+button4.addEventListener("click", () => {
+  mode4Right = !mode4Right;
+
+  img4.src = mode4Right
+    ? "pngs/arrowRight.png"
+    : "pngs/arrowUp.png";
+
+  mode4Right_2 = mode4Right;
+});
+
+button5.addEventListener("click", () => {
+  mode5Right = !mode5Right;
+
+  img5.src = mode5Right
+    ? "pngs/arrowDown.png"
+    : "pngs/arrowRight.png";
+
+  mode5Right_2 = mode5Right;
+});
+
+button6.addEventListener("click", () => {
+  mode6Right = !mode6Right;
+
+  img6.src = mode6Right
+    ? "pngs/arrowDown.png"
+    : "pngs/arrowRight.png";
+
+  mode6Right_2 = mode6Right;
+});
+
+button7.addEventListener("click", () => {
+  mode7Right = !mode7Right;
+
+  img7.src = mode7Right
+    ? "pngs/arrowDown.png"
+    : "pngs/arrowRight.png";
+
+  mode7Right_2 = mode7Right;
+});
+
+button8.addEventListener("click", () => {
+  mode8Right = !mode8Right;
+
+  img8.src = mode8Right
+    ? "pngs/arrowUp.png"
+    : "pngs/arrowLeft.png";
+
+  mode8Right_2 = mode8Right;
+});
+
+button9.addEventListener("click", () => {
+  mode9Right = !mode9Right;
+
+  img9.src = mode9Right
+    ? "pngs/arrowDown.png"
+    : "pngs/arrowLeft.png";
+
+  mode9Right_2 = mode9Right;
 });
 
 updateDecisionPoints();
 
 window.addEventListener("resize", updateDecisionPoints);
 
-
+///*
 // ================= GREEN ================= 
 function animate() {
   if (gameOver || !gameStarted) return;
 
     planeGreen1.style.visibility = "visible";
 
-  if (!verticalLock) {
-    y += speed;
+  if (!verticalLockGreen) {
+    greenY += speed;
 
-    if (!firstDecisionMade && y >= decisionPoint1) {
-      firstDecisionMade = true;
+    if (!firstDecisionMadeGreen && greenY >= decisionPoint1) {
+      firstDecisionMadeGreen = true;
 
       if (!mode1Right) {
         triggerGameOver();
@@ -88,8 +222,8 @@ function animate() {
       }
     }
 
-    if (!secondDecisionMade && y >= decisionPoint2) {
-      secondDecisionMade = true;
+    if (!secondDecisionMadeGreen && greenY >= decisionPoint2) {
+      secondDecisionMadeGreen = true;
 
       if (!mode2Right) {
         triggerGameOver();
@@ -97,89 +231,21 @@ function animate() {
       }
     }
 
+    if (!thirdDecisionMadeGreen && greenY >= decisionPoint3) {
+      thirdDecisionMadeGreen = true;
+
+      if (!mode3Right) {
+        triggerGameOver();
+        return;
+      }
+    }
+
   } else {
-    x += speed;
+    greenX += speed;
   }
 
-  planeGreen1.style.transform = verticalLock ? "rotate(90deg)" : "rotate(0deg)";
-  planeGreen1.style.left = x + "px";
-  planeGreen1.style.bottom = y + "px";
+  planeGreen1.style.transform = verticalLockGreen ? "rotate(90deg)" : "rotate(0deg)";
+  planeGreen1.style.left = greenX + "px";
+  planeGreen1.style.bottom = greenY + "px";
   requestAnimationFrame(animate);
-}
-
-// ================= YELLOW =================
-function animate2() {
-  if (gameOver || !gameStarted) return;
-
-  planeYellow1.style.visibility = "visible";
-
-  if (!verticalLock2) {
-    x2 += speed;
-
-    if (!firstDecisionMade2 && x2 >= decisionPoint1) {
-      firstDecisionMade2 = true;
-
-      if (!mode1Right_2) {
-        verticalLock2 = true;
-      }
-      else if (mode1Right_2) {        
-        triggerGameOver();
-      }
-    }
-
-    if (!secondDecisionMade2 && x2 >= decisionPoint2) {
-      secondDecisionMade2 = true;
-
-      if (!mode2Right_2) {
-        verticalLock2 = true;
-      }
-    }
-
-  } else {
-    y2 += speed;
-  }
-
-   planeYellow1.style.transform = verticalLock2 ? "rotate(-90deg)" : "rotate(0deg)";
-   planeYellow1.style.left = x2 + "px";
-   planeYellow1.style.bottom = y2 + "px";
-  requestAnimationFrame(animate2);
-}
-
-// ================= BLUE =================
-function animate3() {
-  if (gameOver || !gameStarted) return;
-
-  planeBlue1.style.visibility = "visible";
-
-  if (!verticalLock3) {
-    x3 += speed;
-
-    if (!firstDecisionMade3 && x3 >= decisionPoint1) {
-      firstDecisionMade3 = true;
-
-      if (!mode1Right_2) {
-        verticalLock3 = true;
-        triggerGameOver();
-      }
-    }
-
-    if (!secondDecisionMade3 && x3 >= decisionPoint2) {
-      secondDecisionMade3 = true;
-
-      if (!mode2Right_2) {
-        verticalLock3 = true;
-      }
-      else if (mode2Right_2) {
-        triggerGameOver();
-      }
-    }
-
-  } else {
-    y3 += speed;
-  }
-
-  planeBlue1.style.transform = verticalLock3 ? "rotate(-90deg)" : "rotate(0deg)";
-  planeBlue1.style.left = x3 + "px";
-  planeBlue1.style.bottom = y3 + "px";
-  requestAnimationFrame(animate3);
 }
