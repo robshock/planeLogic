1f109552b6cdfeb12e95defd10e5893455b2405b
robshock <robshock16@gmail.com>
2026-06-01 22:25:34 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -72,13 +72,13 @@ function updateScore() {
   scoreDisplay.textContent = `Score: ${score}`;
 }
 // SETTINGS
-let decisionPoint1 = window.innerHeight * 0.39;
-let decisionPoint2 = window.innerHeight * 0.52;
-let decisionPoint3 = window.innerHeight * 0.63;
+let decisionPoint1 = window.innerHeight * 0.42;
+let decisionPoint2 = window.innerHeight * 0.55;
+let decisionPoint3 = window.innerHeight * 0.66;
 let decisionPoint4 = window.innerWidth * 0.525;
-let decisionPoint5 = window.innerHeight * 0.63;
-let decisionPoint6 = window.innerHeight * 0.52;
-let decisionPoint7 = window.innerHeight * 0.41;
+let decisionPoint5 = window.innerHeight * 0.56;
+let decisionPoint6 = window.innerHeight * 0.45;
+let decisionPoint7 = window.innerHeight * 0.34;
 let decisionPoint4yellow = window.innerHeight * 0.72;
 let decisionPoint5yellow = window.innerHeight * 0.61;
 let decisionPoint6yellow = window.innerHeight * 0.50;
