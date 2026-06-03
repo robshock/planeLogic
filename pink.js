ff3573d3b77b1d550c94c42a6269fdb31e21c052
robshock <robshock16@gmail.com>
2026-06-02 21:56:42 -0600

    Add files via upload

--- pink.js
+++ pink.js
@@ -10,8 +10,8 @@ let fifthDecisionMadePink = false;
 let sixthDecisionMadePink = false;
 let seventhDecisionMadePink = false;
 
-const PinkYLimit = window.innerHeight * 0.80;
-const PinkYLimit2 = window.innerHeight * 0.66;
+const PinkYLimit = window.innerHeight * 0.83;
+const PinkYLimit2 = window.innerHeight * 0.69;
 
 const PinkXLimit = window.innerWidth * 0.525;
 const PinkXLimit2 = window.innerWidth * 0.62;
